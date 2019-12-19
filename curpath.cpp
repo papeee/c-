e0d678208af6aeb2ab5a1fe7b6aaa4d1f73e1417
papeee <57752844+papeee@users.noreply.github.com>
2019-12-19 17:08:10 +0200

    Update curpath.cpp

--- curpath.cpp
+++ curpath.cpp
@@ -1,14 +1,8 @@
 // curpath.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-//https://cboard.cprogramming.com/cplusplus-programming/23754-importing-class-dllimport.html
-//#pragma comment(lib,"mcpath.lib")
 #include "pch.h"
 #include <iostream>
 #include <windows.system.h>
 #include <windows.h>
-#ifndef bsolwindow_h
-#define bsolwindow_h
-#endif
 #include <time.h>
 
 //Load the DLL
@@ -50,16 +44,3 @@ int main()
 	curPath.CallDll();
 	return 0;
 }
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
